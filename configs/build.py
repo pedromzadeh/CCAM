334d2844fcb3c042b5bfa5b1ddc529b9096e39ae
Pedrom Zadeh <zadeh@virginia.edu>
2022-10-13 12:21:54 -0400

    define a single cell with appropriate params

--- configs/build.py
+++ configs/build.py
@@ -5,14 +5,14 @@ import os
 
 pol_type = sys.argv[1]
 
-betas = np.linspace(4, 10, 6)
-gammas = np.linspace(0.9, 1.8, 7)
-As = np.linspace(0.32, 0.64, 11)
+betas = np.linspace(4, 10, 1)
+gammas = np.linspace(0.9, 1.8, 1)
+As = np.linspace(0.32, 0.64, 1)
 SIZE = len(betas) * len(gammas) * len(As)
 grid = np.meshgrid(betas, gammas, As)
 varied_cell_tuples = list(zip(grid[0].flatten(), grid[1].flatten(), grid[2].flatten()))
 
-default_cell_tup = (6, 1.26, 0.48)
+default_cell_tup = (6, 0.9, 0.48)
 default_cell_tuples = list((default_cell_tup,) * SIZE)
 
 print(f"speed: {betas} \ngamma: {gammas}\nsubA: {As}\n")
@@ -20,14 +20,14 @@ print(f"Total # of runs: {SIZE}")
 
 tuples = [default_cell_tuples, varied_cell_tuples]
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{pol_type}")
-centers = [[6, 9], [43, 9]]
+centers = [[12.5, 25], [43, 9]]
 
 for id in range(len(varied_cell_tuples)):
     path = os.path.join(root, f"grid_id{id}")
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
 
-    for cell_id in range(2):
+    for cell_id in range(1):
         beta, gamma, A = list(map(float, tuples[cell_id][id]))
         config = dict(
             id=cell_id,
@@ -39,8 +39,8 @@ for id in range(len(varied_cell_tuples)):
             R_init=3.5,
             eta=0.5,
             N_wetting=500,
-            g=50,
-            D=0.01,
+            g=2,
+            D=0.002,
             J=3,
             lam=0.8,
             polarity_mode=str(pol_type).upper(),

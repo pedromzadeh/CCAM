8448dd9a0fd805f22fe9324663f515953f884dce
Pedrom Zadeh <zadeh@virginia.edu>
2022-11-22 16:50:33 -0500

    build grid search the old way

--- configs/build.py
+++ configs/build.py
@@ -1,10 +1,12 @@
 import sys
 import yaml
 import os
+import numpy as np
 
 pol_type = sys.argv[1]
 
-SIZE = 5
+subset = 30
+SIZE = subset * 3
 
 # beta, gamma, A
 default_cell_tup = (500, 0.9, 0.48)
@@ -13,7 +15,17 @@ default_cell_tuples = list((default_cell_tup,) * SIZE)
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{pol_type}")
 centers = [12.5, 25]
 
-for id in range(SIZE):
+# grid search over cell size and angular noise
+Ds = [0.05, 0.1, 0.5]
+Rs = [3.5]
+
+Ds = [[e] * subset for e in Ds]
+Rs = [[e] * SIZE for e in Rs]
+
+Ds = np.array(Ds).reshape(-1)
+Rs = np.array(Rs).reshape(-1)
+
+for id, D, R in list(zip(range(SIZE), Ds, Rs)):
     path = os.path.join(root, f"grid_id{id}")
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
@@ -25,13 +37,13 @@ for id in range(SIZE):
         gamma=gamma,
         A=A,
         beta=beta,
-        R_eq=3.5,
+        R_eq=float(R),
         R_init=3.5,
         eta=0.5,
         N_wetting=500,
         g=2,
         alpha=0.5,
-        D=0.01,
+        D=float(D),
         J=3,
         lam=0.8,
         polarity_mode=str(pol_type).upper(),

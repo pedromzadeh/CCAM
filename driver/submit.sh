240dad796201533fbe9f9438592ba826ca122fee
Pedrom Zadeh <zadeh@virginia.edu>
2022-11-22 18:13:47 -0500

    no longer need a time delay since seed time independent

--- driver/submit.sh
+++ driver/submit.sh
@@ -5,10 +5,11 @@ echo "Finished removing output. Now running 48 collisions per submission"
 
 # To seed each grid_id differently based on time, sleep for 1s when submitting
 # grid_id jobs
-for i in {0..89}
-do
-    sbatch --array=$i job.slurm
-    sleep 1
-done
+# for i in {0..89}
+# do
+#     sbatch --array=$i job.slurm
+#     sleep 1
+# done
 
+sbatch --array=0-$GID job.slurm
 sqme

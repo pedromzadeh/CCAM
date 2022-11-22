7c2636c2036d78e63ea9e88629db678404d636ac
Pedrom Zadeh <zadeh@virginia.edu>
2022-11-22 16:51:36 -0500

    make multiple videos at once

--- driver/sort_img.py
+++ driver/sort_img.py
@@ -3,19 +3,21 @@ from glob import glob
 
 import numpy as np
 
-files = glob("../output/integrins/grid_id0/run_0/visuals/*.png")
-ids = [int(f.split("/")[-1].split("_")[1].split(".")[0]) for f in files]
+for grid_id in [100, 200, 300, 624, 148, 867]:
 
-sort_indx = np.argsort(ids)
+    files = glob(f"../output/integrins/grid_id{grid_id}/run_0/visuals/*.png")
+    ids = [int(f.split("/")[-1].split("_")[1].split(".")[0]) for f in files]
 
-for i, indx in enumerate(sort_indx):
-    curr_file = files[indx]
-    new_file = f"../output/integrins/grid_id0/run_0/visuals/img_{i}.png"
-    cmd = f"mv {curr_file} {new_file}"
+    sort_indx = np.argsort(ids)
+
+    for i, indx in enumerate(sort_indx):
+        curr_file = files[indx]
+        new_file = f"../output/integrins/grid_id{grid_id}/run_0/visuals/img_{i}.png"
+        cmd = f"mv {curr_file} {new_file}"
+        os.system(cmd)
+
+    print("Making a movie...")
+    cmd = f"ffmpeg -i ../output/integrins/grid_id{grid_id}/run_0/visuals/img_%d.png \
+        -b:v 4M -s 500x500 -pix_fmt yuv420p mov_{grid_id}.mp4"
     os.system(cmd)
-
-print("Making a movie...")
-cmd = "ffmpeg -i ../output/integrins/grid_id0/run_0/visuals/img_%d.png \
-    -b:v 4M -s 2500x2500 -pix_fmt yuv420p integrins.mp4"
-os.system(cmd)
-print("Done!")
+    print("Done!")

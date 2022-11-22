3e25ed95be35f68a32f578a824adc83ac27f845f
Pedrom Zadeh <zadeh@virginia.edu>
2022-11-22 17:14:05 -0500

    sleep 1s when submitting grid_id jobs so each gets a different seed due to time

--- driver/submit.sh
+++ driver/submit.sh
@@ -2,5 +2,13 @@ echo "Removing output/"
 rm ../output -rf
 
 echo "Finished removing output. Now running 48 collisions per submission"
-sbatch --array=0-89 job.slurm
+
+# To seed each grid_id differently based on time, sleep for 1s when submitting
+# grid_id jobs
+for i in {0..89}
+do
+    sbatch --array=$i job.slurm
+    sleep 1
+done
+
 sqme

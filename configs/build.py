f7b7d8f76026e548e3bf33c9bc4b7bb1fd79a9aa
Pedrom Zadeh <zadeh@virginia.edu>
2023-01-11 13:14:39 -0500

    refactor via sklearn ParameterGrid

--- configs/build.py
+++ configs/build.py
@@ -2,6 +2,8 @@ import numpy as np
 import yaml
 import os
 
+from sklearn.model_selection import ParameterGrid
+
 pol_type = "IM"
 N_cells = 1
 
@@ -10,43 +12,44 @@ N_cells = 1
 #   - tau   : timescale of pol field decay
 #   - D     : magnitude of noise
 
-betas = np.linspace(1, 1, 1)
-taus = np.linspace(1, 1, 1)
-Ds = np.linspace(0.1, 0.1, 1)
-SIZE = len(betas) * len(taus) * len(Ds)
-grid = np.meshgrid(betas, taus, Ds)
-varied_cell_tuples = list(zip(grid[0].flatten(), grid[1].flatten(), grid[2].flatten()))
+betas = np.linspace(1, 2, 2)
+taus = np.linspace(1, 2, 2)
+Ds = np.linspace(0.1, 0.2, 2)
+
+default_dict = {
+    "center": [[12.5, 25]],
+    "gamma": [1.0],
+    "A": [0],
+    "R_eq": [4],
+    "R_init": [4],
+    "eta": [0.5],
+    "g": [2],
+    "lam": [0.8],
+    "N_wetting": [500],
+    "alpha": [1],
+    "tau_mp": [1],
+    "id": [0],
+    "polarity_mode": [str(pol_type).upper()],
+}
+
+param_grid = {
+    "betas": list(map(float, betas)),
+    "taus": list(map(float, taus)),
+    "Ds": list(map(float, Ds)),
+} | default_dict
+
+grid = list(ParameterGrid(param_grid))
 
 print(f"betas: {betas} \ntaus: {taus}\nDs: {Ds}\n")
-print(f"Total # of runs: {SIZE}")
+print(f"Total # of configs: {len(grid)}")
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{pol_type}")
 
-for id in range(SIZE):
+for id, params in enumerate(grid):
     path = os.path.join(root, f"grid_id{id}")
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
 
-    for cell_id in range(N_cells):
-        beta, tau, D = list(map(float, varied_cell_tuples[id]))
-        config = dict(
-            center=[12.5, 25],
-            gamma=1.0,
-            A=0,
-            R_eq=4,
-            R_init=4,
-            eta=0.5,
-            g=2,
-            lam=0.8,
-            N_wetting=500,
-            alpha=1,
-            tau_mp=1,
-            id=cell_id,
-            polarity_mode=str(pol_type).upper(),
-            beta=beta,
-            tau=tau,
-            D=D,
-        )
-
-        with open(os.path.join(path, f"cell{cell_id}.yaml"), "w") as yfile:
-            yaml.dump(config, yfile)
+    with open(os.path.join(path, f"cell{0}.yaml"), "w") as yfile:
+        print(type(params))
+        yaml.dump(params, yfile)

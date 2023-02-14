4d31d7ecd7cde9ddcf0a120fef99defa4e4a5aa4
Pedrom Zadeh <zadeh@virginia.edu>
2023-02-14 12:31:17 -0500

    correct paths

--- _server/submit.sh
+++ _server/submit.sh
@@ -1,15 +1,2 @@
-# echo "Removing output/"
-# rm ../output -rf
-
-# echo "Finished removing output. Now running 48 collisions per submission"
-
-# To seed each grid_id differently based on time, sleep for 1s when submitting
-# grid_id jobs
-# for i in {0..89}
-# do
-#     sbatch --array=$i job.slurm
-#     sleep 1
-# done
-
 sbatch --array=$1-$2 job.slurm
 sqme
